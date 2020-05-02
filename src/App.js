c0c9c9fc4c7888d672504bf4c0c6462664df5040
Dmytro Hachok <d.gachok@gmail.com>
2020-05-02 21:53:06 +0200

    init app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
+import * as ml5 from 'ml5';
+import Webcam from "react-webcam";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const videoConstraints = {
+    width: 1280,
+    height: 720,
+    facingMode: "user"
+};
+
+const App = () => {
+    const webcamRef = React.useRef(null);
+
+    const classifyImg = () => {
+        // Initialize the Image Classifier method with MobileNet
+        const classifier = ml5.imageClassifier('MobileNet', webcamRef.current, modelLoaded);
+
+        // When the model is loaded
+        function modelLoaded() {
+            console.log('Model Loaded!');
+        }
+
+        // Put the image to classify inside a variable
+        const image = document.getElementById('image');
+        // Make a prediction with a selected image
+        classifier.predict(image, 5, function (err, results) {
+            // print the result in the console
+            console.log(results);
+        })
+    }
+
+    const capture = React.useCallback(
+        () => {
+            console.log('webcamRef.current', webcamRef.current);
+            const imageSrc = webcamRef.current.getScreenshot();
+        },
+        [webcamRef]
+    );
+
+    useEffect(() => {
+        classifyImg();
+    }, []);
+
+    return (
+        <div className="App">
+            <h1>Image classification with ML5.js</h1>
+        </div>
+    );
 }
 
 export default App;
